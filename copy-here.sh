f3bf87d50e05e49c0ced389e50f579b3ce4f975e
Tobias Schulz <tobias.schulz186@gmail.com>
2014-08-07 13:41:08 +0200

    my cars

---
+++ copy-here.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for x in /data/Dropbox/Apps/My\ Cars*/*
+do
+	export date=$(LC_ALL=C stat "$x" | awk '/Access/{print $2 $3}' | tail -n1  | tr -d'' '-' | cut -d"." -f1 | tr -d'' ':')
+	mv "$x" MyCars/mycar_data_$date.xml
+done
