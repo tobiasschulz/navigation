90efaa34ca84f2db800d7b5c68fa0718e4e7bef8
Tobias Schulz <tobias.schulz186@gmail.com>
2014-08-07 13:41:53 +0200

    latest my cars backup

--- copy-here.sh
+++ copy-here.sh
@@ -5,3 +5,5 @@ do
 	export date=$(LC_ALL=C stat "$x" | awk '/Access/{print $2 $3}' | tail -n1  | tr -d'' '-' | cut -d"." -f1 | tr -d'' ':')
 	mv "$x" MyCars/mycar_data_$date.xml
 done
+git add --all MyCars
+git commit -a -m "latest my cars backup"
